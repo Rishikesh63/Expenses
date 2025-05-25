20a836fadcadd9be2608ba896c9303d12038daf4
Rishikesh Maddhesiya <rishi89324@gmail.com>
2025-05-25 15:55:23 +0530

    enhance ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,25 +9,25 @@ let dummy_expense=[
         id:"e1",
         tittle:"school fee",
         amount:250,
-        date:new Date(2023,2,12)
+        date:new Date(2025,4,25)
     },
     {
         id:"e2",
         tittle:"book fee",
         amount:220,
-        date:new Date(2023,2,4)
+        date:new Date(2025,4,25)
     },
     {
         id:"e3",
         tittle:"food fee",
         amount:200,
-        date:new Date(2023,2,8)
+        date:new Date(2025,4,25)
     },
     {
         id:"e4",
         tittle:"room fee",
         amount:2500,
-        date:new Date(2023,2,10)
+        date:new Date(2025,4,25)
     }
     ]
 
